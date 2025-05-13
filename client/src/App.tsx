2f47e1462487ba97908933cf9cc60f15bfc72879
lartikonj <lartikonj@gmail.com>
2025-05-13 17:05:50 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+// client/src/App.tsx
 import { Switch, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import Subject from "@/pages/Subject";
 import Article from "@/pages/Article";
+import About from "@/pages/About";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -12,6 +14,7 @@ function Router() {
       <Route path="/" component={Home} />
       <Route path="/subject/:slug" component={Subject} />
       <Route path="/article/:slug" component={Article} />
+      <Route path="/about" component={About} />
       <Route component={NotFound} />
     </Switch>
   );
