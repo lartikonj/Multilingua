c41af44cb52606bb707eecfb4a34d3bf02d8a630
lartikonj <lartikonj@gmail.com>
2025-05-14 03:16:41 +0100

    Update Article.tsx

--- client/src/pages/Article.tsx
+++ client/src/pages/Article.tsx
@@ -29,8 +29,9 @@ export default function ArticlePage() {
   const translation = article?.translations[language as keyof typeof article.translations] || 
                      article?.translations.en;
   const [match, params] = useRoute("/subject/:subjectSlug/:slug");
-  const subjectSlug = params?.subjectSlug;
   const slug = params?.slug || "";
+  const subjectSlug = params?.subjectSlug;
+
 
   if (!match) return <NotFound />;
   return (
